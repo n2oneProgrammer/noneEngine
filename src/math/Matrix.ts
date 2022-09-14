77cd84c93ddfd7f33902eff56e929bc23fc92da5
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-09-14 19:32:11 +0200

    all matrix

--- src/math/Matrix.ts
+++ src/math/Matrix.ts
@@ -55,4 +55,88 @@ export default class Matrix {
         }
         return new Matrix(result);
     }
+
+    cut(row: number, col: number): Matrix {
+        let result: number[][] = [];
+        let index = 0;
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                if (x == col || y == row) continue;
+                let targetY = Math.floor(index / (this.cols - 1));
+                let targetX = index % (this.cols - 1);
+                if (result[targetY] == null) result[targetY] = [];
+                result[targetY][targetX] = this.values[y][x];
+                index++;
+            }
+        }
+        return new Matrix(result);
+    }
+
+    minor(): Matrix {
+        if (this.rows == 0 && this.cols == 0) {
+            return new Matrix([[this.values[1][1], this.values[1][0]],
+                [this.values[0][1], this.values[0][0]]])
+        }
+        let result: number[][] = [];
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                if (result[y] == null) result[y] = [];
+                let det = this.cut(y, x).determinant();
+                if (det == null) throw new Error("I cannot calculate minor");
+                result[y][x] = det;
+            }
+        }
+        return new Matrix(result);
+    }
+
+    cofactor(): Matrix {
+        let result = this.minor();
+        for (let y = 0; y < result.rows; y++) {
+            for (let x = 0; x < result.cols; x++) {
+                result.values[y][x] *= (x + y) % 2 == 0 ? 1 : -1;
+            }
+        }
+        return result;
+    }
+
+    adjugate(): Matrix {
+        return this.cofactor().transpose();
+    }
+
+    determinant(): number | null {
+        if (this.rows == 2 && this.cols == 2) {
+            return this.determinant2x2();
+        }
+        if (this.rows == this.cols) {
+            let cof = this.cofactor();
+            let result = 0;
+            for (let j = 0; j < this.cols; j++) {
+                result += this.values[0][j] * cof.get(j, 0);
+            }
+            return result;
+        }
+        return null;
+    }
+
+    private determinant2x2(): number {
+        return this.values[0][0] * this.values[1][1] - this.values[0][1] * this.values[1][0];
+    }
+
+    inverse(): Matrix | null {
+        if (this.rows != this.cols) throw new Error("Inverse Matrix can be only from square matrix");
+        let det = this.determinant();
+        if (det == 0 || det == null) return null;
+        return this.adjugate().mul(1 / det);
+    }
+
+    static makeIdentity(size: number): Matrix {
+        let result: number[][] = [];
+        for (let y = 0; y < size; y++) {
+            for (let x = 0; x < size; x++) {
+                if (result[y] == null) result[y] = [];
+                result[y][x] = x == y ? 1 : 0;
+            }
+        }
+        return new Matrix(result);
+    }
 }
